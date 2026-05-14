ecc02d176a53fb50be53e755890c8a0d41de2113
Parshu108 <parshuramsahanivictory@gmail.com>
2026-05-14 12:51:08 +0530

    fix: correct package versions and rename Blog to blog

--- app/component/Navbar.js
+++ app/component/Navbar.js
@@ -58,6 +58,7 @@ const Navbar = () => {
             <li className="relative group">
               <Link
                 href="/blogs"
+                prefetch={false}
                 className="text-[#0bccc1]/90 font-semibold transition duration-300 hover:text-[#07ebd1]"
               >
                 Blog
@@ -133,7 +134,7 @@ const Navbar = () => {
             Services
           </Link>
           <Link
-            href="/blog"
+            href="/blogs"
             className="block text-[#0bccc1] hover:text-[#07ebd1] transition"
           >
             Blog

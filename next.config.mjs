11d28ce4e57ad472f31b0da6f3606d2d402e8125
Parshu108 <parshuramsahanivictory@gmail.com>
2026-05-14 10:12:00 +0530

    comment

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  theme: {
-    extend: {
-      colors: {
-        primary: "var(--primary-bg)",
-        secondary: "var(--secondary-color)",
-        highlight: "var(--highlight-color)",
-        card: "var(--card-bg)",
-        muted: "var(--text-muted)",
-        borderCustom: "var(--border-color)",
-      },
-    },
-  },
+  
 };
 
 export default nextConfig;

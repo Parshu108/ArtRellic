2a43788929aecffd9046ac7abe585edc0a48847e
Parshu108 <parshuramsahanivictory@gmail.com>
2026-05-14 10:15:49 +0530

    comment2

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,19 @@
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": {
+      theme: {
+        extend: {
+          colors: {
+            primary: "var(--primary-bg)",
+            secondary: "var(--secondary-color)",
+            highlight: "var(--highlight-color)",
+            card: "var(--card-bg)",
+            muted: "var(--text-muted)",
+            borderCustom: "var(--border-color)",
+          },
+        },
+      },
+    },
   },
 };
 
